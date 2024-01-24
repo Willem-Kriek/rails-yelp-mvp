530e2c6a5353c2f5e679326f846a5e0d6f833830
willem kriek <kriek.wj@gmail.com>
2024-01-24 14:37:00 +0100

    added restaurant routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+ocean_basket = { name: 'Ocean Basket', address: '7 Boundary St, London', phone_number: '0126489', category: 'belgian' }
+pizza_east = { name: 'Pizza East', address: '28 River St, Berlin', phone_number: '0186789', category: 'french' }
+bella_donna = { name: 'Bella Donna', address: '10 Orange St, Cape Town', phone_number: '0196784', category: 'italian' }
+grillhouse = { name: 'Grillhouse', address: '29 Road St, Leipzig', phone_number: '0167948', category: 'japanese' }
+nuri = { name: 'Nuri', address: '69 Peter St, NY', phone_number: '0189475', category: 'japanese' }
+
+[ocean_basket, pizza_east, bella_donna, grillhouse, nuri].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
